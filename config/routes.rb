40a1e8fbb4f0553ff39b31f7253f4d0709fb6a24
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-04-24 19:40:27 +0200

    added user profile route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-
+  resources :users, only: [:show]
+  
   root 'posts#index'
 
   resources :posts do
