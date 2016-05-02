0fd002bd97cfbba6881014b37bb9b594e1e08884
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-05-02 22:42:47 +0200

    fixed missing avatar urls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
       "notice" => "alert-info"
     }[flash_type] || flash_type.to_s
   end
+  
+  def get_user_avatar_url_for(user, size)
+    if user.avatar.exists?(size)
+      user.avatar.url(size)
+    else
+      '/images/missing_avatar.png'
+    end
+  end
 end
