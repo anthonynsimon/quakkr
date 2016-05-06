e74705d91b5324d7cc1750cf1abc5734a3d64302
Anthony N. Simon <anthonynsimon@gmail.com>
2016-05-06 16:16:08 +0200

    enabled suggested config for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -150,8 +150,8 @@ Devise.setup do |config|
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
-  # config.email_regexp = /\A[^@]+@[^@]+\z/
-
+  config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+    
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
