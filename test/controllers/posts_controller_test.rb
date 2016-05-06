312167a261f1ba9a74ebab3db93278a6b527b954
Anthony N. Simon <anthonynsimon@gmail.com>
2016-05-06 18:13:17 +0200

    reset public repo tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,34 +1,4 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  setup do
-    @post = posts(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:posts)
-  end
-
-  test "should create post" do
-    assert_difference('Post.count') do
-      post :create, post: { message: @post.message }
-    end
-
-    assert_redirected_to post_path(assigns(:post))
-  end
-
-  test "should show post" do
-    get :show, id: @post
-    assert_response :success
-  end
-
-  test "should destroy post" do
-    assert_difference('Post.count', -1) do
-      delete :destroy, id: @post
-    end
-
-    assert_redirected_to posts_path
-  end
 end
