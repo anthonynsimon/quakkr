1a6d740f02d8623389cd09dc157f72b192b93f87
Anthony N. Simon <anthonynsimon@gmail.com>
2016-05-23 18:47:16 +0200

    partially implemented profiles_controller tests

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
-describe ProfilesController do
+describe ProfilesController, :type => :controller do
   describe 'GET #show' do
-    it 'assings @posts of user in path'
-    it 'renders the show template'
+    it 'assings the requested user to @user' do 
+      user = FactoryGirl.create(:user)
+      get :show, user_name: user.user_name
+      expect(assigns(:user)).to eq(user)
+    end
+    
+    it 'assings posts of user in path' do
+      user = FactoryGirl.create(:user)
+      post = FactoryGirl.create(:post, user_id: user.id)
+      get :show, user_name: user.user_name
+      expect(assigns(:user).posts).to include(post)
+    end
+    
+    it 'renders the show template' do
+      get :show, user_name: FactoryGirl.create(:user).user_name
+      expect(response).to render_template(:show)
+    end
+    
     it 'renders the not found template if no user was found'
   end
   
