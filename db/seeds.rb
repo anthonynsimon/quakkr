07ffcc94606851af4c95c7b88e57cc4777831786
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-05-24 11:44:00 +0200

    added db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+random = Random.new
+
+user_names = [ 'amzans', 'cactus_99', 'vgent', 'marius', 'brandon_f', 'dh_andersen' ]
+
+user_names.each do |user_name|
+  User.create(user_name: user_name, email: "#{user_name}@gmail.com", password: Faker::Internet.password, bio: Faker::Lorem.sentence)
+end
+
+user_names.each do |user_name|
+  user = User.find_by(user_name: user_name)
+  random.rand(5..14).times do 
+    Post.create(user: user, message: Faker::Lorem.sentence, created_at: Faker::Date.between(30.days.ago, Date.today))
+  end
+end
+
+user_names.each do |user_name|
+  user = User.find_by(user_name: user_name)
+  random.rand(80..200).times do 
+    Comment.create(user: user, post: Post.find(random.rand(1..Post.count)), body: Faker::Lorem.sentence)
+  end
+end
+
+user_names.each do |user_name|
+  user = User.find_by(user_name: user_name)
+  random.rand(80..200).times do 
+    Post.find(random.rand(1..Post.count)).liked_by user
+  end
+end
+
+user_names.each do |user_name|
+    other_user_names = user_names - [user_name]
+    user_a = User.find_by(user_name: user_name)
+    
+    other_user_names.each do |other_user_name|
+      user_b = User.find_by(user_name: other_user_name)
+      Follow.create(follower_id: user_a.id, following_id: user_b.id)
+    end
+end
