6838b17e49f9b8877d816be9047e90f3f8cbb55f
Anthony N. Simon <anthonynsimon@gmail.com>
2016-05-27 14:56:40 +0200

    wrote relationships controller tests

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -1,15 +1,52 @@
 require 'rails_helper'
 
 describe RelationshipsController do
-  describe 'DELETE #destroy' do
-    it 'deletes the @relationship if the user is authorized'
-    it 'rejects the request if the user is not authorized'
-    it 'responds to javascript'
+  before :each do
+    # Needed for redirect_to :back to work
+    request.env["HTTP_REFERER"] = root_path
+    
+    @user = FactoryGirl.create(:user)
+    @other_user = FactoryGirl.create(:user)
+    sign_in @user
   end
-  
+
   describe 'POST #create' do
-    it 'creates a relationship between current user and user in params'
-    it 'rejects the request if the user is not authorized'
-    it 'responds to javascript'
+    it 'creates a relationship between current user and user in params' do
+      expect{
+        post :create, user_name: @other_user.user_name
+      }.to change(Follow, :count).by(1)
+    end
+    
+    it 'redirects the request if the user is not authenticated' do
+      sign_out @user
+      post :create, user_name: @other_user.user_name
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before :each do
+      post :create, user_name: @other_user.user_name
+    end
+  
+    it 'deletes the @relationship if the user is authorized' do
+      expect{
+        delete :destroy, user_name: @other_user.user_name
+      }.to change(Follow, :count).by(-1)
+    end
+    
+    it 'checks for the existence of a relationship before removing it' do
+      expect{
+        2.times do
+          delete :destroy, user_name: @other_user.user_name
+        end
+      }.to change(Follow, :count).by(-1)
+    end
+    
+    it 'redirects the request if the user is not authenticated' do
+      sign_out @user
+      delete :destroy, user_name: @other_user.user_name
+      expect(response).to redirect_to new_user_session_path
+    end
   end
 end
