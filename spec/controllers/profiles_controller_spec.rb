fbc32eaf2a42402f5a7be084a688a5dcfc8318b2
Anthony N. Simon <anthonynsimon@gmail.com>
2016-05-27 10:25:16 +0200

    added profiles tests

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -1,22 +1,24 @@
 require 'rails_helper'
 
 describe ProfilesController, :type => :controller do
+  before :each do
+    @user = FactoryGirl.create(:user)
+  end
+    
   describe 'GET #show' do
     it 'assings the requested user to @user' do 
-      user = FactoryGirl.create(:user)
-      get :show, user_name: user.user_name
-      expect(assigns(:user)).to eq(user)
+      get :show, user_name: @user.user_name
+      expect(assigns(:user)).to eq(@user)
     end
     
     it 'assings posts of user in path' do
-      user = FactoryGirl.create(:user)
-      post = FactoryGirl.create(:post, user_id: user.id)
-      get :show, user_name: user.user_name
+      post = FactoryGirl.create(:post, user_id: @user.id)
+      get :show, user_name: @user.user_name
       expect(assigns(:user).posts).to include(post)
     end
     
     it 'renders the show template' do
-      get :show, user_name: FactoryGirl.create(:user).user_name
+      get :show, user_name: @user.user_name
       expect(response).to render_template(:show)
     end
     
@@ -24,22 +26,59 @@ describe ProfilesController, :type => :controller do
   end
   
   describe 'GET #edit' do
-    it 'assings @user to edit'
-    it 'renders the edit template'
-    it 'rejects the request if the user is not authenticated'
-    it 'rejects the request if the user is not authorized'
-  end
-  
-  describe 'DELETE #destroy' do
-    it 'deletes the @post if the user is authorized'
-    it 'rejects the request if the user is not authorized'
-    it 'responds to javascript'
-    it 'responds to html'
+    it 'assings @user to edit' do
+      sign_in @user
+      get :edit, user_name: @user.user_name
+      expect(assigns(:user)).to eq(@user)
+    end
+    
+    it 'renders the edit template' do
+      sign_in @user
+      get :edit, user_name: @user.user_name
+      expect(response).to render_template(:edit)
+    end
+    
+    it 'redirects the request if the user is not authenticated' do
+      get :edit, user_name: @user.user_name
+      expect(response).to redirect_to new_user_session_path
+    end
+    
+    it 'redirects the request if the user is not authorized' do
+      sign_in @user
+      other_user = FactoryGirl.create(:user)
+      get :edit, user_name: other_user.user_name
+      expect(response).to redirect_to root_path
+    end
   end
   
   describe 'POST #update' do
-    it 'saves the new data for the profile'
-    it 'rejects the request if the user is not authorized'
-    it 'redirects to the #show'
+    before :each do
+      @new_bio = Faker::Lorem.sentence
+    end
+  
+    it 'assings @user to update' do
+      sign_in @user
+      post :update, user_name: @user.user_name, user: { bio: @new_bio }
+      expect(assigns(:user)).to eq(@user)
+    end
+    
+    it 'updates the @user if the user is authenticated and authorized' do
+      sign_in @user
+      post :update, user_name: @user.user_name, user: { bio: @new_bio }
+      expect(assigns(:user).bio).to eq(@new_bio)
+    end
+    
+    it 'redirects the request if the user is not authenticated' do
+      post :update, user_name: @user.user_name, user: { bio: @new_bio }
+      expect(response).to redirect_to new_user_session_path
+    end
+    
+    it 'redirects the request if the user is not authorized' do
+      sign_in @user
+      other_user = FactoryGirl.create(:user)
+      post :update, user_name: other_user.user_name, user: { bio: @new_bio }
+      expect(assigns(:user).bio).to_not eq(@new_bio)
+      expect(response).to redirect_to root_path
+    end
   end
 end
