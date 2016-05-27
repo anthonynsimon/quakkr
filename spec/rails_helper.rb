934ac9b4aaf0634fbea9cb19471bd63c9bd7150a
Anthony N. Simon <anthonynsimon@gmail.com>
2016-05-27 10:25:10 +0200

    include and config devise for tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,8 +3,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require 'spec_helper'
 require 'rspec/rails'
+require 'devise'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -54,6 +57,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
+  config.include Devise::TestHelpers, :type => :controller
 end
 
 Shoulda::Matchers.configure do |config|
