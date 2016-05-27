43faa590993e8f542c089f8b9b46d5c9f87a8f74
Anthony N. Simon <anthonynsimon@gmail.com>
2016-05-27 10:24:55 +0200

    added bio

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,9 @@ require 'faker'
 
 FactoryGirl.define do
   factory :user do |f|
-    f.user_name { Faker::Internet.user_name(4..26).gsub(/[.]/, '') }
+    f.user_name { Faker::Internet.user_name(4..20).gsub(/[.]/, '') }
     f.email { Faker::Internet.email }
     f.password { Faker::Internet.password(8, 20) }
+    f.bio { Faker::Lorem.sentence }
   end
 end
