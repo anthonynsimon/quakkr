139f3eeb3f31771d336afd9f7263727f1822ed31
Anthony N. Simon <anthonynsimon@gmail.com>
2016-05-27 12:11:02 +0200

    wrote comments controller tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,23 +1,69 @@
 require 'rails_helper'
 
 describe CommentsController do
-  describe 'GET #index' do
-    it 'assings @post'
-    it 'assings @comments of @post'
-    it 'renders the comments template'
+  before :each do
+    @user = FactoryGirl.create(:user)
+    @post = FactoryGirl.create(:post, user: @user)
+    @comment = FactoryGirl.create(:comment, post_id: @post.id, user_id: @user.id)
+  end
+
+  describe 'GET #index as nested resource' do
+    before :each do
+      get :index, post_id: @post.id
+    end
+  
+    it 'assigns @post' do
+      expect(assigns(:post)).to eq(@post)
+    end
+    
+    it 'assigns @comments of @post' do
+      expect(assigns(:comments)).to include(@comment)
+    end
+    
+    it 'renders the comments template' do
+      expect(response).to render_template(:index)
+    end
   end
   
-  describe 'POST #create' do
-    it 'creates a new comment if the user is logged in'
-    it 'rejects a new comment if the user is not logged in'
-    it 'responds to javascript'
-    it 'responds to html'
+  describe 'POST #create as nested resource' do
+    before :each do
+      @body = Faker::Lorem.sentence
+    end
+    
+    it 'creates a new comment if the user is logged in' do
+      sign_in @user
+      create_comment
+      expect(assigns(:comment).body).to eq(@body)
+    end
+    
+    it 'rejects a new comment if the user is not logged in' do
+      create_comment 
+      expect(response).to redirect_to new_user_session_path
+    end
+    
+    def create_comment
+      post :create, post_id: @post.id, comment: { body: @body }
+    end
   end
   
   describe 'DELETE #destroy' do
-    it 'deletes the @comment if the user is authorized'
-    it 'rejects the request if the user is not authorized'
-    it 'responds to javascript'
-    it 'responds to html'
+    it 'deletes the @comment if the user is authorized' do
+      sign_in @user
+      expect { 
+        delete :destroy, post_id: @post.id, id: @comment
+      }.to change(Comment, :count).by(-1)
+    end
+    
+    it 'redirects the request if the user is not authenticated' do
+     delete :destroy, post_id: @post.id, id: @comment
+     expect(response).to redirect_to new_user_session_path
+    end
+    
+    it 'does not delete comment is user is not authorized' do
+      sign_in FactoryGirl.create(:user)
+      expect {
+        delete :destroy, post_id: @post.id, id: @comment
+      }.to_not change(Comment, :count)
+    end
   end
 end
